02af9ed52ab5b6e34cd892ec4b5d2be16291fff0
Mike <Mike@DESKTOP-LN6D7LS>
2020-08-30 23:42:12 -0500

    Se agrega cambios para consultas tipo rest

---
+++ cap4web/src/test/java/ec/udla/prueba/cap4web/Cap4webApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.udla.prueba.cap4web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap4webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
